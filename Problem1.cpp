28b6f58f1886a00ccb3b561c4301b54f27728080
Anuj Tripathi <114897668+codegeekanuj@users.noreply.github.com>
2022-10-06 13:12:18 +0530

    Update Problem1.cpp

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,1 +1,10 @@
+//We're starting out by printing the most famous computing phrase of all time! In the editor below, use either printf or cout to print the string Hello, World! to stdout.
 
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
